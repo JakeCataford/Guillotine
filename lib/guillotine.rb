0e93c2f3ed825f36edc9e2abc982d083349a907b
JakeCataford <JakeCataford@Gmail.com>
2014-01-06 10:21:57 -0500

    Fix dat regex...

--- lib/guillotine.rb
+++ lib/guillotine.rb
@@ -2,7 +2,7 @@ require "guillotine/engine"
 
 module Guillotine
   def self.execute opts, command_string
-    command_array = command_string.downcase.gsub(/[^a-z\s]-_//, '').split ' '
+    command_array = command_string.downcase.gsub(/[^a-z\s\-\_]/i, '').split ' '
     begin
       excecutor = Object.const_get("Executors").const_get(command_array.shift.capitalize)
       response = excecutor.send(command_array.shift.downcase.to_sym, opts, *command_array)
